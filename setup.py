2c6436bec2babb23ed214a3a5dd5e408476c62a7
yngtodd <young.todd.mk@gmail.com>
2019-07-22 21:58:45 -0400

    Remove long description

--- setup.py
+++ setup.py
@@ -13,19 +13,11 @@ if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
     sys.exit()
 
-readme = open('README.rst').read()
-doclink = """
-Documentation
--------------
-
-The full documentation is at http://datastore.rtfd.org."""
-history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
-    name='datastore',
+    name='bsec_datastore',
     version='0.1.0',
     description='Data store for Bthe BSEC Pilot 3 program.',
-    long_description=readme + '\n\n' + doclink + '\n\n' + history,
     author='Todd Young',
     author_email='youngmt1@ornl.gov',
     url='https://github.com/yngtodd/datastore',
