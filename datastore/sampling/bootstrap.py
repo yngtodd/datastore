158ca3041393359f31bfd5938f325cb572f79337
yngtodd <young.todd.mk@gmail.com>
2019-07-26 16:14:21 -0400

    Add to docstrings + comments

--- datastore/sampling/bootstrap.py
+++ datastore/sampling/bootstrap.py
@@ -8,15 +8,35 @@ from datastore.api.data import Subset
 
 
 def bootstrap(dataset, num_bootstraps, prop_train=0.5, seed=13):
-    """ Create bootstrap samples """
+    """ Create bootstrap samples 
 
+    Parameters
+    ----------
+    dataset : datastore.dataset 
+
+    num_bootstraps : int
+        Number of bootstrap samples
+
+    prop_train : float
+        Proportion of samples to make up the training set
+    
+    seed : int
+        Random seed to control the sampling
+
+    Returns
+    -------
+    samples : list(namedtuple<Subset, Subset>)
+        bootstrap samples of the data
+    """
     BootstrapSample = namedtuple('BootstrapSample', 'train valid')
+    # Lean towards a slightly larger training set
     n_samples = math.ceil(len(dataset) * prop_train)
     data_idx = [x for x in range(len(dataset))]
 
     samples = []
     for i in range(num_bootstraps):
         train_idx = resample(data_idx, n_samples=n_samples)
+        # Get dataset indices not included in training sample 
         valid_idx = np.setdiff1d(data_idx, train_idx)
 
         sample = BootstrapSample(
