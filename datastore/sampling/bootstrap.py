7ddd32f0a65ce4c5c1b367cb5e8cabcdcdffb736
yngtodd <young.todd.mk@gmail.com>
2019-07-31 13:47:34 -0400

    Add simple sample method

--- datastore/sampling/bootstrap.py
+++ datastore/sampling/bootstrap.py
@@ -7,6 +7,18 @@ from sklearn.utils import resample
 from datastore.api.data import Subset
 
 
+def dummy_indices(dataset):
+    """ Get indexes for the dataset """
+    return [x for x in range(len(dataset))]
+
+
+def sample(dataset, num_samples, replace=True):
+    """ Sample the dataset """
+    data_idx = dummy_indices(dataset)
+    sample_idx = resample(data_idx, n_samples=num_samples, replace=replace)
+    return Subset(dataset, sample_idx)
+
+
 def leave_one_out_bootstrap(dataset, num_bootstraps, prop_train=0.5, seed=13):
     """ Create bootstrap samples 
 
@@ -31,7 +43,7 @@ def leave_one_out_bootstrap(dataset, num_bootstraps, prop_train=0.5, seed=13):
     BootstrapSample = namedtuple('BootstrapSample', 'train test')
     # Lean towards a slightly larger training set
     n_samples = math.ceil(len(dataset) * prop_train)
-    data_idx = [x for x in range(len(dataset))]
+    data_idx = dummy_indices(dataset) 
 
     samples = []
     for i in range(num_bootstraps):
