e05776889665ed6d9806a31b979cb117513c8356
yngtodd <young.todd.mk@gmail.com>
2019-08-13 23:32:56 -0400

    Fix docstring

--- datastore/api/data.py
+++ datastore/api/data.py
@@ -62,11 +62,11 @@ class InMemoryDataset(Dataset):
         return pd.DataFrame(data_dict)
 
     def to_csv(self, path):
-        " Save the data to disk "
+        """ Save the data to disk """
         self.dataframe().to_csv(path, index=False)
 
     def load_cached(self, path):
-        " Load the data from disk "
+        """ Load the data from disk """
         frame = pd.read_csv(path)
 
         self.data = frame.pop('data')
